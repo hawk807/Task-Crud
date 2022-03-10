a72816c598328df39fd725c4ea5b3eb3caff2634
AmeerHamza <ameer.hamza@akdesigner.com>
2022-03-10 13:36:11 +0500

    redirected user to tasks route by default

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\TaskController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/tasks');
 });
 
 Route::resource('tasks', TaskController::class);
